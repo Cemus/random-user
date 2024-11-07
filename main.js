2923488dc705583a147ce50061bbb9a64e98de02
Cemus <24947582+Cemus@users.noreply.github.com>
2024-11-07 16:53:26 +0100

    Base layout

--- main.js
+++ main.js
@@ -1,1 +1,62 @@
 import "./style.css";
+
+async function getData() {
+  const url = "https://randomuser.me/api/";
+  try {
+    const response = await fetch(url, { method: "GET" });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    return json.results[0];
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function createUser() {
+  const parentElement = document.getElementById("app");
+  while (parentElement.firstChild) {
+    parentElement.removeChild(parentElement.lastChild);
+  }
+  let personData = await getData();
+  let nameData = `${personData.name.title} ${personData.name.first} ${personData.name.last}`;
+  let addressData = `Address : ${personData.location.street.number} - ${personData.location.street.name} (${personData.location.city}, ${personData.location.country}) `;
+
+  const cardElement = document.createElement("div");
+  cardElement.classList.add("card");
+
+  const imageElement = document.createElement("img");
+  imageElement.src = personData.picture.large;
+  imageElement.alt = nameData;
+
+  const nameElement = document.createElement("h3");
+  nameElement.textContent = nameData;
+
+  const emailElement = document.createElement("p");
+  emailElement.textContent = personData.email;
+
+  const addressElement = document.createElement("p");
+  addressElement.textContent = addressData;
+
+  const phoneElement = document.createElement("p");
+  phoneElement.textContent = `Phone : ${personData.phone}`;
+
+  const buttonElement = document.createElement("button");
+  buttonElement.textContent = "Random user";
+  buttonElement.addEventListener("click", async () => {
+    personData = await createUser();
+  });
+
+  cardElement.appendChild(imageElement);
+  cardElement.appendChild(nameElement);
+  cardElement.appendChild(emailElement);
+  cardElement.appendChild(addressElement);
+  cardElement.appendChild(phoneElement);
+  cardElement.appendChild(buttonElement);
+
+  parentElement.appendChild(cardElement);
+}
+
+await createUser();
